d7915688d66f2868c3e6b62898cc73c99fcc46b5
Frances Rather <francesrather@gmail.com>
2019-02-15 13:51:25 -0500

    secondcommit

--- Softland-html/Landing/mail.php
+++ Softland-html/Landing/mail.php
@@ -5,7 +5,7 @@
     $sub= $_POST["subject"];
     $text= $_POST["message"];
 
-    
+
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
@@ -17,7 +17,7 @@
         <tr><td>Email: '.$email.'</td></tr>
         <tr><td>Subject: '.$subject.'</td></tr>
         <tr><td>Text: '.$text.'</td></tr>
-        
+
     </table>';
 
     if (@mail($to, $email, $message, $headers))
