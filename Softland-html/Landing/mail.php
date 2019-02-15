e80f87e5ee4d262183611c11cf3c318953386824
Frances Rather <francesrather@gmail.com>
2019-02-15 11:56:14 -0500

    firstcommit

---
+++ Softland-html/Landing/mail.php
@@ -0,0 +1,30 @@
+<?php
+    $to = 'demo@yoursite.com';
+    $name = $_POST["name"];
+    $email= $_POST["email"];
+    $sub= $_POST["subject"];
+    $text= $_POST["message"];
+
+    
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    $message ='<table style="width:100%">
+        <tr>
+            <td>'.$firstname.'</td>
+        </tr>
+        <tr><td>Email: '.$email.'</td></tr>
+        <tr><td>Subject: '.$subject.'</td></tr>
+        <tr><td>Text: '.$text.'</td></tr>
+        
+    </table>';
+
+    if (@mail($to, $email, $message, $headers))
+    {
+        echo 'The message has been sent.';
+    }else{
+        echo 'failed';
+    }
+
+?>
